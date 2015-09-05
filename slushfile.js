dc36b39808a8941bbabe99fb34c993c2e7df3278
Tyler Ipson <tyler.ipson@gmail.com>
2015-09-05 08:35:21 -0600

    Removing rename

--- slushfile.js
+++ slushfile.js
@@ -1,18 +1,9 @@
-/*
- * slush-colony
- * https://github.com/loremipson/slush-colony
- *
- * Copyright (c) 2015, Tyler Ipson
- * Licensed under the MIT license.
- */
-
 'use strict';
 
 var gulp = require('gulp'),
   install = require('gulp-install'),
   conflict = require('gulp-conflict'),
   template = require('gulp-template'),
-  rename = require('gulp-rename'),
   _ = require('underscore.string'),
   inquirer = require('inquirer'),
   path = require('path');
